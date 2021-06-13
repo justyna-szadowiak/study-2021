7ad43e351bfbc7bbe0feeb7b68b9ad22ddbd0346
Justyna Szadowiak <justyna.elzbieta.szadowiak@gmail.com>
2021-06-13 17:18:50 +0200

    Removed exercie 001

--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,10 @@ import ReactDOM from 'react-dom';
 
 
 const App = () => {
-    const buttonText = {text: 'Click me'};
     function getTime() { return (new Date()).toLocaleTimeString() }
 
     return ( 
     <div>
-        <label className = "label" htmlFor = "name" > 
-        Enter name: 
-        </label> 
-        <input id = "name" type = "text" />
-        <button style = {{ backgroundColor: 'blue', color: 'white' }}> 
-            {buttonText.text}
-        </button>  
-
         <div>
             Current time:
         </div>
