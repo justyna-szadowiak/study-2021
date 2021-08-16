4b82f5fc2ab859b2081010d249ab24562099659b
Justyna Szadowiak <justyna.elzbieta.szadowiak@gmail.com>
2021-08-16 16:12:15 +0200

    Season app which uses user latitude

--- src/ExtraData.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ExtraData = (props) => {
-  return <div className="ui placeholder segment">{props.children}</div>;
-};
-
-export default ExtraData;
--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ExtraData from "./ExtraData";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-const App = () => {
-  return (
-    <div>
-      <ExtraData>
-        <div className="ui icon header">
-          <i className="pdf file outline icon"></i>
-          No document are listed for this customer.
-        </div>
-        <div className="ui primary button">Add document</div>
-      </ExtraData>
-      <ExtraData>
-        <h4 class="ui header">For Your Information</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur, adipisci elit, sed eius modi
-          tempora incidunt, ut labore et dolore magna aliqua. Ut enim ad minima
-          veniam, quis nostru exercitation ullam co laborios.
-        </p>
-      </ExtraData>
-    </div>
-  );
-};
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please accept location request" />;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
