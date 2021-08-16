db2e9ed8fc22f235c8ca589f7af67023aefe0f25
Justyna Szadowiak <justyna.elzbieta.szadowiak@gmail.com>
2021-08-16 17:03:48 +0200

    Small app for showing local time

--- src/index.jsx
+++ src/index.jsx
@@ -1,31 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from "faker";
-import CommentDetail from "./CommentDetail";
 
-const App = () => {
-  return (
-    <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 7.45AM"
-        comment="Great!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Yesterday at 9:35PM"
-        comment="Good job!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jim"
-        timeAgo="Yesterday at 6:05PM"
-        comment="Very interesting"
-        avatar={faker.image.avatar()}
-      />
-    </div>
-  );
-};
+class Clock extends React.Component {
+  state = { time: "" };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ time: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
+
+  render() {
+    return <div className="time">The time is: {this.state.time}</div>;
+  }
+}
+
+ReactDOM.render(<Clock />, document.querySelector("#root"));
